81391ea7634c23bdfdb76f859b5feb149e3b15c3
Hannahpohmah <111054206+Hannahpohmah@users.noreply.github.com>
2023-11-12 21:18:31 +0000

    Update my_streamlit_app.py

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
 import tensorflow as tf
 import numpy as np
 import pickle
